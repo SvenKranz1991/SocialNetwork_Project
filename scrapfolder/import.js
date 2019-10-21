b327e4a248db490b58ca2392b96cc3d33de7e757
Sven Kranz <svenkranz@t-online.de>
2019-10-21 20:37:42 +0200

    GitIfnored Folder

--- scrapfolder/import.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-const store = createStore(
-    reducer,
-    composeWithDevTools(applyMiddleware(reduxPromise))
-);
